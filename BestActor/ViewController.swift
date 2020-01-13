c0173bf23cd34fd40ad37e4536fb08b9613026da
Mike Zhu <12051594@life.hkbu.edu.hk>
2020-01-13 23:58:20 +0800

    Initial setup for the Best Actor project
    Create Xcode project and gitignore file
    
    [CV-FinalProject-iOS]

---
+++ BestActor/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BestActor
+//
+//  Created by Mike's Macbook on 1/13/20.
+//  Copyright © 2020 Mikemikezhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
