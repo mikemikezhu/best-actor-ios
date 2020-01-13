5d2da05ba8884a8505559b67445a8950bad5e95d
Mike Zhu <12051594@life.hkbu.edu.hk>
2020-01-14 00:42:51 +0800

    Restructure and add backward compatibility for app delegate and scene delegate
    1. Restructure in delegate folder
    2. Add backward compatibility for app delegate and scene delegate
    
    [CV-FinalProject-iOS]

---
+++ BestActor/Delegates/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  BestActor
+//
+//  Created by Mike's Macbook on 1/13/20.
+//  Copyright © 2020 Mikemikezhu. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	@available(iOS 13.0, *)
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		// Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+		// If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+		// This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+	}
+}
