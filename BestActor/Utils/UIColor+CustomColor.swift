7a873497d5d1b3df4f296457ea7ba013891d650b
Mike Zhu <12051594@life.hkbu.edu.hk>
2020-01-14 01:00:35 +0800

    Update custom background color and status bar style
    1. Create base view controller
    2. Create custom background color in UIColor extensions
    3. Set custom background color
    4. Set view controller-based status bar appearance to NO in info.plist
    5. Set status bar style to light content
    
    [CV-FinalProject-iOS]

---
+++ BestActor/Utils/UIColor+CustomColor.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+CustomColor.swift
+//  BestActor
+//
+//  Created by Mike's Macbook on 1/14/20.
+//  Copyright © 2020 Mikemikezhu. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+	class var customBackgroundColor: UIColor {
+		return UIColor.init(red: 135.0/255.0,
+							green: 203.0/255.0,
+							blue: 235.0/255.0,
+							alpha: 1.0)
+	}
+}
---
+++ BestActor/Views/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  BestActor
+//
+//  Created by Mike's Macbook on 1/14/20.
+//  Copyright © 2020 Mikemikezhu. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Set background color
+		view.backgroundColor = UIColor.customBackgroundColor
+	}
+}
