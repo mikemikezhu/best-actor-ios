2a88212927fd1583b05758a228e76761f4a0eabc
Mike Zhu <12051594@life.hkbu.edu.hk>
2020-01-17 14:23:33 +0800

    Create the logic to randomly display facial expression
    [CV-FinalProject-iOS]

---
+++ BestActor/Views/FacialExpression/BAFacialExpressionDiscriptionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BAFacialExpressionDiscriptionViewController.swift
+//  BestActor
+//
+//  Created by Mike's Macbook on 1/17/20.
+//  Copyright © 2020 Mikemikezhu. All rights reserved.
+//
+
+import UIKit
+
+class BAFacialExpressionDiscriptionViewController: BABaseViewController {
+
+	@IBOutlet weak var facialExpressionLabel: BATitleLabel!
+	@IBOutlet weak var facialExpressionImageView: UIImageView!
+
+	override func viewDidLoad() {
+
+		super.viewDidLoad()
+
+		// Load facial expression
+		let identifier = String(describing: BAFacialExpressionService.self)
+		let service = BACoreServiceFactory.sharedFactory.findService(identifier)
+
+		if let facialExpressionService = service as? BAFacialExpressionService {
+
+			if let facialExpression = facialExpressionService.loadFacialExpression() {
+				print("[INFO] - Load facial expression: \(facialExpression)")
+				configureFacialExpressionLabel(facialExpression)
+				configureFacialExpressionImageView(facialExpression)
+			}
+		}
+	}
+
+	private func configureFacialExpressionLabel(_ facialExpression: String) {
+
+		facialExpressionLabel.text = facialExpression.uppercased()
+	}
+
+	private func configureFacialExpressionImageView(_ facialExpression: String) {
+
+		facialExpressionImageView.image = UIImage(named: facialExpression)
+	}
+}
